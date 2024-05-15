d9b96987b14dd13ba49c240593c805f8d7446583
Sadafsajad <sadafsajad451@gmail.com>
2024-05-15 10:31:08 +0530

    created mvc pattern

---
+++ Controllers/Apps.php
@@ -0,0 +1,11 @@
+<?php
+// YourController.php
+
+class Apps
+{
+    public function index()
+    {
+        // Load the view
+        include 'Views/index.php';
+    }
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Include your controller file
+require_once 'Controllers/Apps.php';
+
+// Create an instance of your controller
+$controller = new Apps();
+
+// Call a method on your controller to handle the request
+$controller->index();
+?>
